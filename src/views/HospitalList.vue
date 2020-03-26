273742c4096767322c5ad846f751291b141d4f20
Michael Weaver <mjweaver01@gmail.com>
2020-03-25 21:56:25 -0400

    Add pagination, remove package lock from gitignore

--- src/views/HospitalList.vue
+++ src/views/HospitalList.vue
@@ -1,23 +1,32 @@
 <template>
-    <div class="hospital-list-container basic-page-container">
-        <h1>FIND A HOSPITAL THAT NEEDS FACEMASKS!</h1>
-        <router-link tag="button" to="/hospital-input">Add your hospital!</router-link>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</p>
-        <div class="list-container">
-            <div class="list-item list-header">
-                <div class="name">Hospital</div>
-                <div class="location">Location</div>
-                <div class="address">Address</div>
-                <div class="need">Need</div>
-            </div>
-            <div class="list-item" v-for="{ name, city, address, facemaskNeed } in hospitals" :key="name">
-                <div class="name">{{ name }}</div>
-                <div class="location">{{ city }}</div>
-                <div class="address">{{ address }}</div>
-                <div class="need">{{ facemaskNeed }}</div>
-            </div>
-        </div>
+  <div class="hospital-list-container basic-page-container">
+    <h1>FIND A HOSPITAL THAT NEEDS FACEMASKS!</h1>
+    <router-link tag="button" to="/hospital-input">Add your hospital!</router-link>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</p>
+    <div class="list-container">
+      <div class="list-item list-header">
+        <div class="name">Hospital</div>
+        <div class="location">Location</div>
+        <div class="address">Address</div>
+        <div class="need">Need</div>
+      </div>
+      <div class="list-item" v-for="{ name, city, address, facemaskNeed } in currentHospitalsPage" :key="name">
+        <div class="name">{{ name }}</div>
+        <div class="location">{{ city }}</div>
+        <div class="address">{{ address }}</div>
+        <div class="need">{{ facemaskNeed }}</div>
+      </div>
     </div>
+    <div class="pagination" v-if="paginatedHospitalsLength > 1">
+      <button
+        v-for="page in paginatedHospitalsLength"
+        :key="page"
+        @click="goToPage(page)"
+      >
+        {{ page }}
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -25,10 +34,60 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'Sewing',
+
+  data () {
+    return {
+      currentPage: 0,
+      pageSize: 3
+    }
+  },
+
   computed: {
     ...mapState('hospitals', [
       'hospitals'
-    ])
+    ]),
+
+    paginatedHospitals () {
+      // Create array from Object
+      let hospitalsArray = []
+
+      if (Object.keys(this.hospitals).length > 0) {
+        Object.keys(this.hospitals).forEach(hospital => {
+          hospitalsArray.push(this.hospitals[hospital])
+        })
+      }
+
+      // Splice array into chunks
+      if (hospitalsArray.length > 0) {
+        return hospitalsArray.reduce((resultArray, item, index) => {
+          const chunkIndex = Math.floor(index / this.pageSize)
+
+          if (!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = []
+          }
+
+          resultArray[chunkIndex].push(item)
+
+          return resultArray
+        }, [])
+      }
+
+      return hospitalsArray
+    },
+
+    currentHospitalsPage () {
+      return this.paginatedHospitals[this.currentPage]
+    },
+
+    paginatedHospitalsLength () {
+      return this.paginatedHospitals.length
+    }
+  },
+
+  methods: {
+    goToPage (pageIndex) {
+      this.currentPage = pageIndex - 1
+    }
   }
 }
 </script>
