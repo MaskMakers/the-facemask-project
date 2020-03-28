6566fde341eb4bf9e57daf15bc0fb176746e32fe
Andrew Haas <ahaas3@apple.com>
2020-03-27 19:24:41 -0700

    update masks, add mask detail

--- src/views/MakeMask.vue
+++ src/views/MakeMask.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="masks-container basic-page-container">
+  <div class="make-a-mask-container basic-page-container">
     <h1>Make A Mask</h1>
     <h2>Choose A Mask Template</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     <div class="masks">
-      <div class="mask" v-for="{ id, name, subtitle, description, steps } in masks" :key="id">
-        <p>{{ id }}</p>
-        <p>{{ name }}</p>
-        <p>{{ subtitle }}</p>
-        <p>{{ description }}</p>
-        <p>{{ steps }} step(s)</p>
+      <div class="mask" v-for="{ id, name, subtitle, description } in masks" :key="id">
+        <router-link :to="'/mask/' + id">
+          <div :class="'image image-' + id"></div>
+          <div class="copy">
+            <h3 class="name">{{name}}</h3>
+            <p class="subtitle">{{ subtitle }}</p>
+            <p class="description">{{ description }}</p>
+          </div>
+        </router-link>
       </div>
     </div>
   </div>
@@ -23,27 +26,49 @@ export default {
   components: {},
   computed: {
     ...mapState('tabletop', [
-      'masks',
-      'steps'
+      'masks'
     ])
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.masks-container {
-  margin-top: 50px;
+.make-a-mask-container {
+  margin: 50px auto 100px;
 }
 
 .masks {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-gap: 5%;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 3%;
   padding-top: 1em;
+  margin-top: 50px;
+
+  @media screen and (max-width: $bp-m) {
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 4%;
+  }
 }
 
 .mask {
-  background: $gray;
-  padding: 1em;
+  text-align: left;
+
+  a {
+    color: $secondary-color;
+  }
+}
+
+.image {
+  width: 100%;
+  height: 300px;
+  background-color: $gray-light;
+}
+
+.name {
+  margin-bottom: 5px;
+}
+
+.subtitle {
+  margin-top: 0;
 }
 </style>
