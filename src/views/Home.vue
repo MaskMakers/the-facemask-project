8408b2acaa2d0c26bfaab4b08206a837ccef3e29
Michael Weaver <mjweaver01@gmail.com>
2020-03-27 20:47:26 -0400

    Update homepage links and text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,13 @@
     <h1 class="visuallyhidden">The Facemask Project</h1>
     <h2>Make a facemask at home. Send it to a hospital that&nbsp;needs&nbsp;one.</h2>
     <div class="hero-links-container">
-      <router-link to="/sewing-page" class="hero-link">
+      <router-link to="/make-a-mask" class="hero-link">
         <div class="image"></div>
-        <div class="link-text">I AM QUARANTINED AND I HAVE A SEWING&nbsp;MACHINE!</div>
+        <h2 class="link-text">MAKE A MASK</h2>
       </router-link>
-      <router-link to="/hospital-list" class="hero-link">
+      <router-link to="/send-a-mask" class="hero-link">
         <div class="image"></div>
-        <div class="link-text">I WORK IN A HOSPITAL AND DON’T HAVE A&nbsp;FACEMASK!</div>
+        <h2 class="link-text">SEND A MASK</h2>
       </router-link>
     </div>
     <div class="featured-info">
