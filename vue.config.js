0c7ea1c1b4619159b8f06c25dde002a9f9827b2b
Michael Weaver <mjweaver01@gmail.com>
2020-03-27 20:38:29 -0400

    Whitelist VH, add mixins to global prepend

--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,10 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import "@/scss/shared/_variables.scss";`,
+        prependData: `
+          @import "@/scss/shared/_variables.scss";
+          @import "@/scss/shared/mixins.scss";
+        `,
         sassOptions: {
           scoped: false
         }
