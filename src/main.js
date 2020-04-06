fe52f7a2b34faf1582ec5849f196622f91c16fe2
Michael Weaver <mjweaver01@gmail.com>
2020-04-05 20:09:08 -0400

    Restructure Vue GTM to fire with proper data

--- src/main.js
+++ src/main.js
@@ -6,11 +6,8 @@ import VueGtm from 'vue-gtm'
 
 Vue.use(VueGtm, {
   id: 'GTM-KZGN2B4',
-  enabled: true,
-  debug: true,
-  loadScript: true,
-  vueRouter: router,
-  ignoredViews: ['homepage']
+  debug: process.env.NODE_ENV !== 'production',
+  loadScript: true
 })
 
 Vue.config.productionTip = false
@@ -20,8 +17,18 @@ Vue.component('vue-image', () => import('@/components/VueImage'))
 Vue.component('vue-background-image', () => import('@/components/VueBackgroundImage'))
 Vue.component('arrow-icon', () => import('@/components/ArrowIcon'))
 
-new Vue({
+const app = new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
+  render: function (h) { return h(App) },
+  mounted () {
+    let current = this.$router.history.current
+    this.$gtm.trackView(current.name, current.path)
+  }
 }).$mount('#app')
+
+// GTM router
+router.beforeEach((to, from, next) => {
+  app.$gtm.trackView(to.name, to.path)
+  next()
+})
