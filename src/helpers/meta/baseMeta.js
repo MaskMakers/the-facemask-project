09a73aca87174fb8ef144e987b908e7ff182d643
Andrew Haas <ahaas3@apple.com>
2020-04-07 08:46:56 -0700

    update meta

--- src/helpers/meta/baseMeta.js
+++ src/helpers/meta/baseMeta.js
@@ -1,7 +1,7 @@
 export default function baseMeta () {
   let baseMeta = {
     title: 'Mask Makers',
-    description: 'The Facemask Project is a grassroots way of helping to protect the medical staffs in our communities through the COVID-19 pandemic, by encouraging ordinary citizens under quarantine (and with a sewing machine) to become heroes of the pandemic!',
+    description: 'Mask Makers is a grassroots way of helping to protect the medical staffs in our communities through the COVID-19 pandemic, by encouraging ordinary citizens under quarantine (and with a sewing machine) to become heroes of the pandemic!',
     keywords: 'coronavirus masks',
     image: '/og_mask-makers.jpg'
   }
