69f0de414edb8c081d25cc6cfea25c042e728f23
Michael Weaver <mjweaver01@gmail.com>
2020-04-07 18:03:00 -0400

    Comment out vuex local

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import tabletop from './modules/tabletop'
-import VuexPersistence from 'vuex-persist'
+// import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
 
-const vuexLocal = new VuexPersistence({
-  storage: window.localStorage,
-  modules: ['tabletop']
-})
+// const vuexLocal = new VuexPersistence({
+//   storage: window.localStorage,
+//   modules: ['tabletop']
+// })
 
 // reset when storage is older than a half hour
-const hours = 0.5
-const now = new Date().getTime()
-const setupTime = localStorage.getItem('setupTime')
-if (setupTime == null) {
-  localStorage.setItem('setupTime', now)
-} else {
-  if (now - setupTime > hours * 60 * 60 * 1000) {
-    window.localStorage.removeItem('vuex')
-    localStorage.setItem('setupTime', now)
-  }
-}
+// const hours = 0.5
+// const now = new Date().getTime()
+// const setupTime = localStorage.getItem('setupTime')
+// if (setupTime == null) {
+//   localStorage.setItem('setupTime', now)
+// } else {
+//   if (now - setupTime > hours * 60 * 60 * 1000) {
+//     window.localStorage.removeItem('vuex')
+//     localStorage.setItem('setupTime', now)
+//   }
+// }
 
 // only add vuex local on production
 let localPlugins = []
-if (process.env.NODE_ENV === 'production') localPlugins.push(vuexLocal.plugin)
+// if (process.env.NODE_ENV === 'production') localPlugins.push(vuexLocal.plugin)
 
 export default new Vuex.Store({
   state: {
